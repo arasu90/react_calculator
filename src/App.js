6eea20480f5e466c15992d461de0945dc83917a5
Kalaiarasu B <mbmk90@gmail.com>
2024-10-17 12:19:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// src/App.js
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+
+  const handleClick = (value) => {
+    setInput((prev) => prev + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      // eslint-disable-next-line no-eval
+      const result = eval(input);
+      setInput(result.toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <h1>React Calculator</h1>
+      <input type="text" value={input} readOnly />
+      <div className="buttons">
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('+')}>+</button>
+
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('*')}>*</button>
+
+        <button onClick={handleClear}>C</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={handleCalculate}>=</button>
+        <button onClick={() => handleClick('/')}>/</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
